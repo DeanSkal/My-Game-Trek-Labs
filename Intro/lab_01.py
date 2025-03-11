7519b88b3ff6a68a1fa1a1fdfcdfa02ead4bec1c
skalrud.dean <skalrud.dean@emanuelschool.nsw.edu.au>
2025-03-11 11:49:09 +1100

    Intro

--- Intro/lab_01.py
+++ Intro/lab_01.py
@@ -1,6 +1,8 @@
 print("Welcome to my project")
-total_questions = 2
+total_questions = 3
 Password = "Bean"
+Password2 = "Lapa"
+Password3 = ""
 
 answer = input("Is your name Dean? (yes/no): ")
 if answer.lower() == 'yes':
@@ -9,11 +11,35 @@ elif answer.lower() == 'no':
     print ('You should not be here')
 else:
     print ('You should not be here')
+print ('')
 if answer.lower() == 'yes':
     answer = input("What is the password: ")
     if Password == answer:
-        print ('Welcome to my project')
+        print ('You may continue')
     elif answer.lower() =='bean':
-        print('Welcome to my project')
+        print('You may continue')
     else:
         print ('you liar')
+
+    if Password == answer:
+        print ('')
+        answer = input("What is the second password: ")
+        if Password2 == answer:
+            print('Welcome to my project')
+        elif answer.lower() == 'lapa':
+            print('Welcome to my project')
+        else:
+            print('you very smart liar')
+    elif answer.lower() == 'bean':
+        print('')
+        answer = input("What is the second password: ")
+        if Password2 == answer:
+            print('You may continue')
+        elif answer.lower() == 'lapa':
+            print('You may continue')
+        else:
+            print('you very smart liar')
+
+
+
+
